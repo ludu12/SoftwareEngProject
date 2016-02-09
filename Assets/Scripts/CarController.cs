3b1460b4b08dc05a7877c5066b3dafbd0eb72f8a
jchuipek <jordan-chuipek@uiowa.edu>
2016-02-09 15:21:27 -0600

    Basic Car Controller and Test Map

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour 
+{
+    float speed = 0f;
+    float maxSpeed = 20f;
+    float accel = .2f;
+	void Update () 
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            StartCoroutine(OnForward());
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            StartCoroutine(OnRight());
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            StartCoroutine(OnDown());
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            StartCoroutine(OnLeft());
+	}
+	
+    IEnumerator OnForward()
+    {
+        speed += accel;
+        if (speed > maxSpeed)
+            speed = maxSpeed;
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        yield return null;
+    }
+    IEnumerator OnRight()
+    {
+       
+        yield return null;
+    }
+    IEnumerator OnLeft()
+    {
+     
+        yield return null;
+    }
+    IEnumerator OnDown()
+    {
+       
+        yield return null;
+    }
+}
