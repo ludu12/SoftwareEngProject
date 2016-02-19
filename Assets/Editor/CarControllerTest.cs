cdd856a8d95caeb01ac2abdf33776adae4906bf4
jchuipek <jordan-chuipek@uiowa.edu>
2016-02-19 12:00:50 -0600

    changed gitignore file

---
+++ Assets/Editor/CarControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using NUnit.Framework;
+
+[TestFixture] 
+public class CarControllerTest
+{
+    public CarController GetController()
+    {
+        return NSubstitute.Substitute.For<CarController>();
+    }
+    [Test]
+    public void Forward()
+    {
+        var controller = GetController();
+        controller.OnForward();
+
+        //Assert.That(controller.speed > 0);
+        //Assert.That(controller.transform.position != controller.oldPos);
+    }
+
+    [Test]
+    public void Right()
+    {
+        var controller = GetController();
+        controller.OnRight();
+    }
+}
