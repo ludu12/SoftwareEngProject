8dd5cbd09ab62fda60ceeba8f94a14c46b684524
Luke Dunscombe <robert-dunscombe@uiowa.edu>
2016-02-19 15:45:17 -0600

    Added main menu stuff

---
+++ Assets/Scripts/MainMenu/Controller/MainMenuController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour {
+
+    private ISceneManager sceneManager;
+
+    // Load the scene
+    public void LoadScene(string scene)
+    {
+        sceneManager.LoadScene(scene);
+    }
+
+    // Set SceneManager controller
+    public void SetSceneManangerController(ISceneManager sceneManager)
+    {
+        this.sceneManager = sceneManager;
+    }
+
+}
---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour, ISceneManager {
+
+    public MainMenuController controller;
+
+    private void OnEnable()
+    {
+        controller = GetComponent<MainMenuController>();
+        controller.SetSceneManangerController(this);
+    }    
+
+    // start button
+    public void OnStartButtonClick()
+    {
+        controller.LoadScene("Game");
+    }
+
+    // how to play
+	public void OnHowToPlayButtonClick()
+	{
+        controller.LoadScene("Controls");
+	}
+
+    // back to menu button
+	public void OnControlsBackButtonClick()
+	{
+        controller.LoadScene("MainMenu");
+	}
+
+    #region ISceneManager implementation
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+    #endregion
+}
