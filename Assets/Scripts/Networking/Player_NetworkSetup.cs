8bc622e7d8b458a6d06ed7c761cf22a35a29a16d
Sarah Mazur <sarah-mazur@uiowa.edu>
2016-03-03 13:06:30 -0600

    added network manager and spawning players

---
+++ Assets/Scripts/Networking/Player_NetworkSetup.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_NetworkSetup : NetworkBehaviour {
+
+	[SerializeField]
+	Camera carCam;
+
+	[SerializeField]
+	AudioListener audioListener;
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			GetComponent<CarMotor>().enabled = true;
+			carCam.enabled = true;
+			carCam.GetComponent<CameraShift>().enabled = true;
+			audioListener.enabled = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
