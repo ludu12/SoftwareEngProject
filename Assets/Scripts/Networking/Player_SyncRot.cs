2ae0875cdd746b6ae621a1c66e087baf70e073f8
Luke Dunscombe <robert-dunscombe@uiowa.edu>
2016-03-07 13:06:21 -0600

    Sync pos and sync rotation scripts, dont know if we need these actually

---
+++ Assets/Scripts/Networking/Player_SyncRot.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Player_SyncRot : NetworkBehaviour
+{
+    [SyncVar]
+    private Quaternion syncRot; // automatically sends this to all clients
+
+    [SerializeField] Transform myTransform;
+    [SerializeField] float lerpRate = 15;
+
+    private Quaternion lastRot;
+    private float rotThreshold = 5f;
+
+    void Update()
+    {
+        LerpRot();
+    }
+
+    void FixedUpdate()
+    {
+        TransmitRot();
+    }
+
+    void LerpRot()
+    {
+        if (!isLocalPlayer)
+        {
+            myTransform.rotation = Quaternion.Lerp(myTransform.rotation, syncRot, Time.deltaTime * lerpRate);
+            Debug.Log(Time.deltaTime.ToString());
+        }
+    }
+
+    [Command]
+    void CmdProvideRotToServer(Quaternion rot)
+    {
+        // this command will be recieved by a client from a server
+        syncRot = rot;
+        //Debug.Log("Command for movement");
+    }
+
+    [Client]
+    void TransmitRot()
+    {
+        // sends my position to the servers
+        if (isLocalPlayer && Quaternion.Angle(myTransform.rotation, lastRot) > rotThreshold)
+        {
+            CmdProvideRotToServer(myTransform.rotation);
+            lastRot = myTransform.rotation;
+        }
+    }
+}
