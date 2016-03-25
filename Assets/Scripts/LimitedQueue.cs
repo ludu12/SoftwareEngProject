a2b296276197c076c3a78c8c0954adcdd9217366
cgename <christopher-gename@uiowa.edu>
2016-03-24 21:33:01 -0500

    starting map_gen

---
+++ Assets/Scripts/LimitedQueue.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+namespace CyberCommon
+{
+    /// <summary>
+    /// Creates queue that has a limit on size.
+    /// It will enqueue until the limit is reached
+    /// then it dequeues the first item.
+    /// Useful for graphing a history of plots.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class LimitedQueue<T> : Queue<T>
+    {
+
+        // limit of the queue
+        private int _limit = -1;
+        public int limit
+        {
+            get { return _limit; }
+            set { _limit = value; }
+        }
+
+        // Constructor sets the limit for this queue and initializes the queue to be the limit
+        public LimitedQueue(int limit) : base(limit)
+        {
+            this.limit = limit;
+        }
+
+        // hide the base enqueue method with new
+        public new void Enqueue(T element)
+        {
+            if (this.Count >= limit)
+            {
+                this.Dequeue();
+            }
+            base.Enqueue(element);
+        }
+    }
+}
---
+++ Assets/Scripts/MapPathGeneration.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using CyberCommon;
+
+public class MapPathGeneration : MonoBehaviour {
+
+    private LimitedQueue<int> mapPath;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void StartupLogic()
+    {
+        mapPath.limit = 15;
+        mapPath.Enqueue(0);
+        for(int i = 0; i < 14; i++)
+        {
+            int r = (rand() % 3) - 1;
+        }
+    }
+
+    void sumMapPath(int i)
+    {
+
+    }
+}
